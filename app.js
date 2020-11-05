7311a3ad4d8136a7659ea6dca7fd2dae91011e58
Mason <mason.fah@gmail.com>
2020-11-05 16:37:35 -0600

    Mongo database added

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 //Routes
 
 app.get('/', (req,res) => {
     res.send('We are on home');
 });
+
+
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true },
+    () => console.log('connected to db'));
 
 //Start listening
 app.listen(3000);
